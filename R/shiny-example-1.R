bae0ecd68d7f2cf8846a298d02e354ee149e7391
Jonathan Adams <jd.adams16@gmail.com>
2018-03-19 22:25:43 -0500

    Add ability to control colors inside app.

--- R/shiny-example-1.R
+++ R/shiny-example-1.R
@@ -11,7 +11,7 @@ library(ggplot2)
 library(shiny)
 
 # Read in data and add identifier for matching county shapes
-censusData <- read.csv('./GitHub/presentation-lsdg-r/R/acs2015_county_data.csv', header = T, stringsAsFactors = F)
+censusData <- read.csv('./Documents/GitHub/presentation-lsdg-r/R/acs2015_county_data.csv', header = T, stringsAsFactors = F)
 censusData$ID <- sprintf('%s,%s', tolower(censusData$State), tolower(censusData$County))
 
 # Load county shapes and join to data set
@@ -20,7 +20,7 @@ censusData <- dplyr::inner_join(x = censusData, y = countyShapes, by = 'ID')
 censusData <- sf::st_as_sf(censusData)
 
 availableStates <- unique(censusData$State)
-availableStates <- c("All States", availableStates)
+# availableStates <- c("All States", availableStates) # Too much for my laptop
 measurements <- names(censusData)
 # Remove identifiers, geometries, and state/county names
 measurements <- measurements[-c(1,2,3,38,39)]
@@ -31,7 +31,9 @@ app <- shinyApp(
     sidebarLayout(
       sidebarPanel(
         selectInput("state", "Choose a state:", choices = availableStates),
-        selectInput("attribute", "Choose data to display:", choices = measurements)
+        selectInput("attribute", "Choose data to display:", choices = measurements)#,
+        #textInput("low", label = "Low Color", value = "orange"),
+        #textInput("high", label = "High Color", value = "blue")
       ),
       mainPanel(
         plotOutput("statePlot")
@@ -47,7 +49,8 @@ app <- shinyApp(
         stateData <- censusData[(censusData$State == input$state),]
       }
       ggplot(stateData) +
-        geom_sf(aes_string(fill = input$attribute))
+        geom_sf(aes_string(fill = input$attribute)) #+
+        #scale_fill_gradient(low = input$low, high = input$high)
     })
   }
 )
